1fbc6eb3b925ef144b05c17daee43b461e943489
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-02-13 21:27:16 +0530

    [Aniket]- Enhanced cab invoice generation

--- CabInvoiceTest/UnitTest1.cs
+++ CabInvoiceTest/UnitTest1.cs
@@ -29,7 +29,17 @@ namespace CabInvoiceTest
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 35.0);
             Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
-
         }
+
+        [Test]
+        public void GivenMultipleRidesShouldReturnInvoiceSummaryAndAverangeFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 65.0);
+            Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
+        }
+
     }
 }
